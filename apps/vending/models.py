4e947ab1cb2b2ef16ad48a5d6c236a111a7ef7ab
Alejandro Vázquez <alejandro.vazquez@abacum.io>
2023-07-21 13:00:32 +0200

    updated_at field added to product model and migration applied

--- apps/vending/models.py
+++ apps/vending/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))])
     created_at = models.DateTimeField(auto_now_add=True)
     stock = models.IntegerField(validators=[MinValueValidator(0)])
+    updated_at = models.DateTimeField(null=True)
 
 class VendingMachineSlot(models.Model):
     class Meta:
