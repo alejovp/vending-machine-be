d4429b44b21417fbeb173a174f23238bcd3986e3
Alejandro Vázquez <alejandro.vazquez@abacum.io>
2023-07-21 17:19:58 +0200

    stock and product models update

--- apps/vending/admin.py
+++ apps/vending/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from apps.vending.models import Product
+from apps.vending.models import Product, Stock
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "price", "created_at", "updated_at"]
+    ordering = ["-created_at"]
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ["product", "quantity", "created_at", "updated_at"]
+    ordering = ["-created_at"]
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Stock, StockAdmin)
--- apps/vending/models.py
+++ apps/vending/models.py
@@ -12,7 +12,10 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=4, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))])
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+         return self.name
 
 class VendingMachineSlot(models.Model):
     class Meta:
@@ -20,11 +23,13 @@ class VendingMachineSlot(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
-    quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+    # quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
     row = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
     column = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
 
 class Stock(models.Model):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+
+    product = models.OneToOneField("Product", unique=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField(null=False, default=0, validators=[MinValueValidator(0)])
+    updated_at = models.DateTimeField(auto_now=True)
