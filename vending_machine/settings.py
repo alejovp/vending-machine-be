f74b1523598fa86bbf78dcd796521f8fc9bc9a9d
Alejandro Vázquez <alejandro.vazquez@abacum.io>
2023-07-21 18:04:27 +0200

    django rest framework added

--- vending_machine/settings.py
+++ vending_machine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
     'apps.health',
     'apps.vending',
 ]
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# rest framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+}
